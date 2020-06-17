bf84e7b419e1a7e1ffaa7ca2189344e0309f6522
James O'Halloran <james.p.ohalloran@gmail.com>
2020-06-17 15:09:03 -0300

    list field def moved out of build Schema

---
+++ packages/forestry-graphql/src/fields/list.ts
@@ -0,0 +1,199 @@
+import {
+  isSectionListField,
+  isNotNull,
+  FieldSourceType,
+  Templates,
+  TemplatePage,
+} from "..";
+import {
+  GraphQLList,
+  GraphQLUnionType,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
+import { friendlyName } from "../plugins";
+import { getData } from "../util";
+import { baseInputFields } from "../inputFields";
+import flatten from "lodash.flatten";
+
+type BaseListField = {
+  label: string;
+  name: string;
+  type: "list";
+};
+type SimpleList = BaseListField & {
+  config: {
+    required?: boolean;
+    use_select: boolean;
+    min: null | number;
+    max: null | number;
+  };
+};
+export type SectionList = BaseListField & {
+  config?: {
+    required?: boolean;
+    use_select: boolean;
+    min: null | number;
+    max: null | number;
+    source: {
+      type: "pages";
+      section: string;
+    };
+  };
+};
+
+export type ListField = SectionList | SimpleList;
+
+export const list = ({
+  fmt,
+  field,
+  rootPath,
+  sectionFmts,
+  templateObjectTypes,
+  templatePages,
+}: {
+  fmt: string;
+  field: ListField;
+  rootPath: string;
+  sectionFmts: {
+    name: string;
+    templates: string[];
+  }[];
+  templateObjectTypes: Templates;
+  templatePages: TemplatePage[];
+}) => {
+  if (isSectionListField(field)) {
+    return {
+      getter: {
+        type: GraphQLList(
+          new GraphQLUnionType({
+            name: friendlyName(field.name + "_list_" + fmt),
+            types: () => {
+              const activeSectionTemplates = sectionFmts.find(
+                ({ name }) => name === field.config?.source.section
+              );
+              const types = activeSectionTemplates?.templates
+                .map(
+                  (templateName: string) => templateObjectTypes[templateName]
+                )
+                ?.filter(isNotNull) || [
+                new GraphQLObjectType({ name: "Woops", fields: {} }), // FIXME fallback to providing a type
+              ];
+
+              return types;
+            },
+            resolveType: async (val) => {
+              return templateObjectTypes[val.template];
+            },
+          })
+        ),
+        resolve: async (val: FieldSourceType) => {
+          let paths = val[field.name];
+          paths = Array.isArray(paths) ? paths : [];
+
+          return await Promise.all(
+            paths.map(async (itemPath) => {
+              const res = await getData<DocumentType>(
+                rootPath + "/" + itemPath
+              );
+              const activeTemplate = templatePages.find(({ pages }) => {
+                return pages?.includes(itemPath);
+              });
+              return {
+                ...res,
+                path: itemPath,
+                template: activeTemplate?.name,
+              };
+            })
+          );
+        },
+      },
+      setter: {
+        type: new GraphQLObjectType({
+          name: friendlyName(field.name + "_list_" + fmt + "_config"),
+          fields: {
+            ...baseInputFields,
+            component: { type: GraphQLString },
+            itemField: {
+              type: new GraphQLObjectType({
+                name: friendlyName(
+                  field.name + "_list_" + fmt + "_config_item"
+                ),
+                fields: {
+                  name: { type: GraphQLString },
+                  label: { type: GraphQLString },
+                  component: { type: GraphQLString },
+                  options: { type: GraphQLList(GraphQLString) },
+                },
+              }),
+            },
+          },
+        }),
+        resolve: () => {
+          const section = field.config?.source.section;
+          const templates = sectionFmts.find(
+            (sectionFmt) => sectionFmt.name === section
+          )?.templates;
+          const possiblePages = flatten(
+            templates?.map((templateName) => {
+              return templatePages?.find(({ name }) => name === templateName)
+                ?.pages;
+            })
+          );
+
+          return {
+            name: field.name,
+            label: field.label,
+            component: "list",
+            itemField: {
+              label: field.label + " Item",
+              name: "path",
+              component: "select",
+              options: possiblePages,
+            },
+          };
+        },
+      },
+      mutator: {
+        type: GraphQLList(GraphQLString),
+      },
+    };
+  }
+
+  return {
+    getter: { type: GraphQLList(GraphQLString) },
+    setter: {
+      type: new GraphQLObjectType({
+        name: friendlyName(field.name + "_list_" + fmt + "_config"),
+        fields: {
+          ...baseInputFields,
+          component: { type: GraphQLString },
+          itemField: {
+            type: new GraphQLObjectType({
+              name: friendlyName(field.name + "_list_" + fmt + "_config_item"),
+              fields: {
+                name: { type: GraphQLString },
+                label: { type: GraphQLString },
+                component: { type: GraphQLString },
+              },
+            }),
+          },
+        },
+      }),
+      resolve: () => {
+        return {
+          name: field.name,
+          label: field.label,
+          component: "list",
+          itemField: {
+            label: field.label + " Item",
+            component: "text",
+          },
+        };
+      },
+    },
+    mutator: {
+      type: GraphQLList(GraphQLString),
+    },
+  };
+};
--- packages/forestry-graphql/src/index.ts
+++ packages/forestry-graphql/src/index.ts
@@ -47,6 +47,7 @@ import { FileField, file } from "./fields/file";
 import { TagListField, tag_list } from "./fields/tagList";
 import { image_gallery, GalleryField } from "./fields/imageGallery";
 import { SelectField, SectionSelect, select } from "./fields/select";
+import { ListField, list, SectionList } from "./fields/list";
 
 type DirectorySection = {
   type: "directory";
@@ -123,33 +124,6 @@ type FieldGroupListField = WithFields & {
     required?: boolean;
   };
 };
-
-type BaseListField = {
-  label: string;
-  name: string;
-  type: "list";
-};
-type SimpleList = BaseListField & {
-  config: {
-    required?: boolean;
-    use_select: boolean;
-    min: null | number;
-    max: null | number;
-  };
-};
-export type SectionList = BaseListField & {
-  config?: {
-    required?: boolean;
-    use_select: boolean;
-    min: null | number;
-    max: null | number;
-    source: {
-      type: "pages";
-      section: string;
-    };
-  };
-};
-type ListField = SectionList | SimpleList;
 
 export type FieldType =
   | TextField
@@ -345,145 +319,6 @@ const buildSchema = async (config: any) => {
       templates,
     }));
 
-  const list = ({ fmt, field }: { fmt: string; field: ListField }) => {
-    if (isSectionListField(field)) {
-      return {
-        getter: {
-          type: GraphQLList(
-            new GraphQLUnionType({
-              name: friendlyName(field.name + "_list_" + fmt),
-              types: () => {
-                const activeSectionTemplates = sectionFmts.find(
-                  ({ name }) => name === field.config?.source.section
-                );
-                const types = activeSectionTemplates?.templates
-                  .map(
-                    (templateName: string) => templateObjectTypes[templateName]
-                  )
-                  ?.filter(isNotNull) || [
-                  new GraphQLObjectType({ name: "Woops", fields: {} }), // FIXME fallback to providing a type
-                ];
-
-                return types;
-              },
-              resolveType: async (val) => {
-                return templateObjectTypes[val.template];
-              },
-            })
-          ),
-          resolve: async (val: FieldSourceType) => {
-            let paths = val[field.name];
-            paths = Array.isArray(paths) ? paths : [];
-
-            return await Promise.all(
-              paths.map(async (itemPath) => {
-                const res = await getData<DocumentType>(
-                  config.rootPath + "/" + itemPath
-                );
-                const activeTemplate = templatePages.find(({ pages }) => {
-                  return pages?.includes(itemPath);
-                });
-                return {
-                  ...res,
-                  path: itemPath,
-                  template: activeTemplate?.name,
-                };
-              })
-            );
-          },
-        },
-        setter: {
-          type: new GraphQLObjectType({
-            name: friendlyName(field.name + "_list_" + fmt + "_config"),
-            fields: {
-              ...baseInputFields,
-              component: { type: GraphQLString },
-              itemField: {
-                type: new GraphQLObjectType({
-                  name: friendlyName(
-                    field.name + "_list_" + fmt + "_config_item"
-                  ),
-                  fields: {
-                    name: { type: GraphQLString },
-                    label: { type: GraphQLString },
-                    component: { type: GraphQLString },
-                    options: { type: GraphQLList(GraphQLString) },
-                  },
-                }),
-              },
-            },
-          }),
-          resolve: () => {
-            const section = field.config?.source.section;
-            const templates = sectionFmts.find(
-              (sectionFmt) => sectionFmt.name === section
-            )?.templates;
-            const possiblePages = flatten(
-              templates?.map((templateName) => {
-                return templatePages?.find(({ name }) => name === templateName)
-                  ?.pages;
-              })
-            );
-
-            return {
-              name: field.name,
-              label: field.label,
-              component: "list",
-              itemField: {
-                label: field.label + " Item",
-                name: "path",
-                component: "select",
-                options: possiblePages,
-              },
-            };
-          },
-        },
-        mutator: {
-          type: GraphQLList(GraphQLString),
-        },
-      };
-    }
-
-    return {
-      getter: { type: GraphQLList(GraphQLString) },
-      setter: {
-        type: new GraphQLObjectType({
-          name: friendlyName(field.name + "_list_" + fmt + "_config"),
-          fields: {
-            ...baseInputFields,
-            component: { type: GraphQLString },
-            itemField: {
-              type: new GraphQLObjectType({
-                name: friendlyName(
-                  field.name + "_list_" + fmt + "_config_item"
-                ),
-                fields: {
-                  name: { type: GraphQLString },
-                  label: { type: GraphQLString },
-                  component: { type: GraphQLString },
-                },
-              }),
-            },
-          },
-        }),
-        resolve: () => {
-          return {
-            name: field.name,
-            label: field.label,
-            component: "list",
-            itemField: {
-              label: field.label + " Item",
-              component: "text",
-            },
-          };
-        },
-      },
-      mutator: {
-        type: GraphQLList(GraphQLString),
-      },
-    };
-  };
-
   const field_group = ({
     fmt,
     field,
@@ -676,7 +511,14 @@ const buildSchema = async (config: any) => {
       case "tag_list":
         return tag_list({ fmt, field });
       case "list":
-        return list({ fmt, field });
+        return list({
+          fmt,
+          field,
+          rootPath: config.rootPath,
+          sectionFmts,
+          templateObjectTypes,
+          templatePages,
+        });
       case "file":
         return file({ fmt, field, rootPath: config.rootPath });
       case "image_gallery":
