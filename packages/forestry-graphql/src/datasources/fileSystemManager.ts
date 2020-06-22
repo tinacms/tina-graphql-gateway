2b0d1252d18a47ca00f8da974da7d8bcaf9a8b68
James O'Halloran <james.p.ohalloran@gmail.com>
2020-06-22 19:35:20 -0300

    Add filesystem rootpath on the provider, so buildSchema can be agnostic

--- packages/forestry-graphql/src/datasources/fileSystemManager.ts
+++ packages/forestry-graphql/src/datasources/fileSystemManager.ts
@@ -1,25 +1,36 @@
 import fs from "fs";
 import matterOrig, { Input, GrayMatterOption } from "gray-matter";
 import { DataSource } from "./datasource";
+import path from "path";
 
 export class FileSystemManager implements DataSource {
+  rootPath: string;
+  constructor(rootPath: string) {
+    this.rootPath = rootPath;
+  }
   getData = async <T>(filepath: string): Promise<T> => {
-    const result = matter(await fs.readFileSync(filepath));
+    const result = matter(await fs.readFileSync(this.getFullPath(filepath)));
 
     // @ts-ignore
     return result;
   };
-  writeData = async <T>(path: string, content: any, data: any) => {
+  writeData = async <T>(filepath: string, content: any, data: any) => {
     const string = stringify(content, data);
-    await fs.writeFileSync(path, string);
 
-    return await this.getData<T>(path);
-  };
-  getDirectoryList = async (path: any) => {
-    const list = await fs.readdirSync(path);
+    const fullPath = this.getFullPath(filepath);
+    await fs.writeFileSync(fullPath, string);
 
-    return list.map((item) => `${path}/${item}`);
+    return await this.getData<T>(fullPath);
   };
+  getDirectoryList = async (filepath: any) => {
+    const list = await fs.readdirSync(this.getFullPath(filepath));
+
+    return list.map((item) => `${filepath}/${item}`);
+  };
+
+  private getFullPath(relPath: string) {
+    return path.join(this.rootPath, relPath);
+  }
 }
 
 const stringify = (content: string, data: object) => {
