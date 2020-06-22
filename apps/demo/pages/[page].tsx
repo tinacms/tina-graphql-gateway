02990396fbbe34ac1318c8f4109f501dff31f1e3
Jeff See <jeffsee.55@gmail.com>
2020-06-22 12:16:04 -0700

    Clean up [page] to rely more on graphql client package

--- apps/demo/pages/[page].tsx
+++ apps/demo/pages/[page].tsx
@@ -1,55 +1,9 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import { useForm, usePlugin } from "tinacms";
-import {
-  onSubmit,
-  prepareValues,
-  rehydrateValues,
-} from "@forestry/graphql-client";
-import { DocumentQueryQuery, BlocksUnion } from "../.forestry/types";
+import { forestryFetch, useForestryForm } from "@forestry/graphql-client";
+import { DocumentUnion, BlocksUnion } from "../.forestry/types";
 import query from "../.forestry/query";
-
-const DocumentQuery = query;
-const API_URL = "http://localhost:4001/graphql";
-
-async function fetchAPI(
-  query: string,
-  { variables }: { variables: { path: string } }
-) {
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  });
-
-  const json = await res.json();
-  if (json.errors) {
-    console.error(json.errors);
-    throw new Error("Failed to fetch API");
-  }
-  return json.data;
-}
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const path = `content/pages/${params.page}.md`;
-  const data = await fetchAPI(DocumentQuery, {
-    variables: { path },
-  });
-
-  const formConfig = {
-    id: `content/pages/${params.page}.md`,
-    label: `content/pages/${params.page}.md`,
-    initialValues: prepareValues(data.document.data),
-    fields: data.document.form.fields,
-  };
-
-  return { props: { path, document: data.document, formConfig } };
-};
 
 export async function getStaticPaths() {
   return {
@@ -62,34 +16,26 @@ export async function getStaticPaths() {
   };
 }
 
-const Home = ({ path, document, formConfig }) => {
-  // TODO: use the form data so changes are immediately reflected
-  const [formData, form] = useForm({
-    ...formConfig,
-    onSubmit: (values) => {
-      onSubmit({ path, payload: values });
-    },
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const path = `content/pages/${params.page}.md`;
+  const response = await forestryFetch<DocumentUnion>({
+    query,
+    path,
   });
-  usePlugin(form);
 
-  const documentWithFormData = rehydrateValues(formData, document);
-
-  return (
-    <div className="">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <PageSwitch document={documentWithFormData} />
-    </div>
-  );
+  return { props: { path, response } };
 };
 
-const PageSwitch = ({
-  document,
-}: {
-  document: DocumentQueryQuery["document"];
-}) => {
+const Home = (props) => {
+  // FIXME: running into issues with multiple instances so passing
+  // useForm by reference rather than importing it
+  const [formData, form] = useForestryForm(props.response, useForm);
+  usePlugin(form);
+
+  return <PageSwitch document={formData} />;
+};
+
+const PageSwitch = ({ document }: { document: DocumentUnion }) => {
   switch (document.__typename) {
     case "BlockPage":
       return <BlockOutput blocks={document.data.blocks} />;
