4540545a2c17bac4cad787ef33fc8bb9eb5a09f8
James O'Halloran <james.p.ohalloran@gmail.com>
2020-06-22 14:35:56 -0300

    Move writeData to filesystem resolver

--- packages/forestry-graphql/src/datasources/fileSystemManager.ts
+++ packages/forestry-graphql/src/datasources/fileSystemManager.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
-import { matter } from "../util";
+import matterOrig, { Input, GrayMatterOption } from "gray-matter";
 
 export interface DataSource {
   getData<T>(filepath: string): Promise<T>;
+  writeData<T>(path: string, content: any, data: any): Promise<T>;
+  getDirectoryList(path: string): Promise<string[]>;
 }
 
 export class FileSystemManager implements DataSource {
@@ -12,4 +14,32 @@ export class FileSystemManager implements DataSource {
     // @ts-ignore
     return result;
   };
+  writeData = async <T>(path: string, content: any, data: any) => {
+    const string = stringify(content, data);
+    await fs.writeFileSync(path, string);
+
+    return await this.getData<T>(path);
+  };
+  getDirectoryList = async (path: any) => {
+    const list = await fs.readdirSync(path);
+
+    return list.map((item) => `${path}/${item}`);
+  };
 }
+
+const stringify = (content: string, data: object) => {
+  return matterOrig.stringify(content, data, {
+    // @ts-ignore
+    lineWidth: -1,
+    noArrayIndent: true,
+  });
+};
+
+const matter = <I extends Input, O extends GrayMatterOption<I, O>>(
+  data: Buffer
+) => {
+  let res;
+  res = matterOrig(data, { excerpt_separator: "<!-- excerpt -->" });
+
+  return res;
+};
--- packages/forestry-graphql/src/util.ts
+++ packages/forestry-graphql/src/util.ts
@@ -1,14 +1,6 @@
 import fs from "fs";
 import matterOrig, { Input, GrayMatterOption } from "gray-matter";
 
-export const stringify = (content: string, data: object) => {
-  return matterOrig.stringify(content, data, {
-    // @ts-ignore
-    lineWidth: -1,
-    noArrayIndent: true,
-  });
-};
-
 export const matter = <I extends Input, O extends GrayMatterOption<I, O>>(
   data: Buffer
 ) => {
@@ -16,24 +8,4 @@ export const matter = <I extends Input, O extends GrayMatterOption<I, O>>(
   res = matterOrig(data, { excerpt_separator: "<!-- excerpt -->" });
 
   return res;
-};
-
-export const getDirectoryList = async (path: any) => {
-  const list = await fs.readdirSync(path);
-
-  return list.map((item) => `${path}/${item}`);
-};
-
-export const getData = async <T>(filepath: string): Promise<T> => {
-  const result = matter(await fs.readFileSync(filepath));
-
-  // @ts-ignore
-  return result;
-};
-
-export const writeData = async (path: string, content: any, data: any) => {
-  const string = stringify(content, data);
-  await fs.writeFileSync(path, string);
-
-  return await getData(path);
 };
