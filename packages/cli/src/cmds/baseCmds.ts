8ddca4015ad1c132b60cf42576b649d6b40a2f8d
James O'Halloran <james.p.ohalloran@gmail.com>
2020-07-14 14:19:35 -0300

    command to generate typescript types

--- packages/cli/src/cmds/baseCmds.ts
+++ packages/cli/src/cmds/baseCmds.ts
@@ -1,13 +1,13 @@
 import { Command } from "../command";
 import { chain } from "../middleware";
-import { login } from "./login";
+import { genTypes } from "./type-gen";
 
-export const CMD_AUTH = "login";
+export const CMD_GEN_TYPES = "types:gen";
 
 export const baseCmds: Command[] = [
   {
-    command: CMD_AUTH,
-    description: "log in to Forestry account",
-    action: (options) => chain([login], options),
+    command: CMD_GEN_TYPES,
+    description: "Generate Typescript types",
+    action: (options) => chain([genTypes], options),
   },
 ];
