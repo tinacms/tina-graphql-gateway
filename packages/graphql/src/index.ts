f6841351b98840d92f2156ada6aede222b9145a4
James O'Halloran <james.p.ohalloran@gmail.com>
2020-07-14 14:29:19 -0300

    Remove typegen from packages outside of cli

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -7,7 +7,5 @@ import type {
   Content,
 } from "./datasources/datasource";
 import { buildSchema } from "./schemaBuilder";
-import { generateTypes } from "./codegen";
-
-export { FileSystemManager, buildSchema, generateTypes };
+export { FileSystemManager, buildSchema };
 export type { DataSource, Settings, FMT, FieldType, Content };
