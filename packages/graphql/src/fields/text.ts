16e061c3285dc75dcf4ace817a72f1c55ca0ed9b
Brent Mitton <bmitton@gatech.edu>
2020-07-16 13:15:39 -0400

    extract text field

---
+++ packages/graphql/src/fields/text.ts
@@ -0,0 +1,38 @@
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+
+import { TextField } from "../datasources/datasource";
+
+const baseInputFields = {
+  name: { type: GraphQLString },
+  label: { type: GraphQLString },
+  description: { type: GraphQLString },
+  component: { type: GraphQLString },
+};
+
+const textInput = new GraphQLObjectType<TextField>({
+  name: "TextFormField",
+  fields: {
+    ...baseInputFields,
+  },
+});
+
+export const text = ({ field }: { fmt: string; field: TextField }) => ({
+  getter: {
+    type: field?.config?.required
+      ? GraphQLNonNull(GraphQLString)
+      : GraphQLString,
+  },
+  setter: {
+    type: textInput,
+    resolve: () => {
+      return {
+        name: field.name,
+        label: field.label,
+        component: field.type,
+      };
+    },
+  },
+  mutator: {
+    type: GraphQLString,
+  },
+});
--- packages/graphql/src/schemaBuilder.ts
+++ packages/graphql/src/schemaBuilder.ts
@@ -421,27 +421,6 @@ export const buildSchema = async (
     },
   });
 
-  const text = ({ field }: { fmt: string; field: TextField }) => ({
-    getter: {
-      type: field?.config?.required
-        ? GraphQLNonNull(GraphQLString)
-        : GraphQLString,
-    },
-    setter: {
-      type: textInput,
-      resolve: () => {
-        return {
-          name: field.name,
-          label: field.label,
-          component: field.type,
-        };
-      },
-    },
-    mutator: {
-      type: GraphQLString,
-    },
-  });
-
   const textarea = ({ field }: { fmt: string; field: TextareaField }) => ({
     getter: {
       type: field?.config?.required
