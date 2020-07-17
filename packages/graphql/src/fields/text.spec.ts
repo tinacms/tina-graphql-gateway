3183c92d080ac796139dab29ae248f31224b2ce5
Brent Mitton <bmitton@gatech.edu>
2020-07-17 10:28:24 -0400

    establishing a testing pattern for fields

--- packages/graphql/src/fields/text.spec.ts
+++ packages/graphql/src/fields/text.spec.ts
@@ -1,4 +1,44 @@
+import { TextField } from "../datasources/datasource";
 import { text } from "./text";
+
+const mockField: TextField = {
+  label: "Field Label",
+  name: "Field Name",
+  default: "Field Default",
+  type: "text",
+};
+const mockFMT = "MockFmt";
+
 describe("Text Field", () => {
-  test("should do something", () => {});
+  describe("Getter", () => {
+    test("should get GraphQL 'String' when not required", () => {
+      const field = text({ fmt: mockFMT, field: mockField });
+      expect(field.getter.type.toString()).toBe("String");
+    });
+
+    test("should get GraphQL 'String!' when required", () => {
+      const field = text({
+        fmt: mockFMT,
+        field: { ...mockField, config: { required: true } },
+      });
+      expect(field.getter.type.toString()).toEqual("String!");
+    });
+  });
+
+  describe("Setter", () => {
+    test("resolves to the correct object", () => {
+      const field = text({ fmt: mockFMT, field: mockField });
+      const resolved = field.setter.resolve();
+      expect(resolved.name).toEqual("Field Name");
+      expect(resolved.label).toEqual("Field Label");
+      expect(resolved.component).toEqual("text");
+    });
+  });
+
+  describe("Mutator", () => {
+    test("is the right type", () => {
+      const field = text({ fmt: mockFMT, field: mockField });
+      expect(field.mutator.type.toString()).toEqual("String");
+    });
+  });
 });
