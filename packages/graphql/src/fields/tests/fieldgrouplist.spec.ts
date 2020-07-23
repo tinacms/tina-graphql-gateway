ae9a4c484bafc020a537ff15fb9faddf15c69e58
Brent Mitton <bmitton@gatech.edu>
2020-07-23 15:05:27 -0400

    stub the tests

---
+++ packages/graphql/src/fields/tests/fieldgrouplist.spec.ts
@@ -0,0 +1,5 @@
+describe("Field Group List Field", () => {
+  describe("Getter", () => {});
+  describe("Setter", () => {});
+  describe("Mutator", () => {});
+});
---
+++ packages/graphql/src/fields/tests/imagegallery.spec.ts
@@ -0,0 +1,5 @@
+describe("Image Gallery Field", () => {
+  describe("Getter", () => {});
+  describe("Setter", () => {});
+  describe("Mutator", () => {});
+});
