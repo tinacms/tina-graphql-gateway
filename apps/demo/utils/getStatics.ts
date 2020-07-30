a9154a94826cd4d456c9f82b77761349aaba8414
Jeff See <jeffsee.55@gmail.com>
2020-07-30 13:55:47 -0700

    Refactor demo app, add posts/authors routes

---
+++ apps/demo/utils/getStatics.ts
@@ -0,0 +1,29 @@
+import { ForestryClient } from "@forestryio/client";
+import fg from "fast-glob";
+import { DocumentUnion, DocumentInput } from "../.forestry/types";
+
+function fileToUrl(template, filepath: string) {
+  filepath = filepath.split(`/${template}/`)[1];
+  return filepath.replace(/ /g, "-").slice(0, -3).trim();
+}
+
+export const getStaticPathsUtil = async ({ template }) => {
+  const items = await fg(`./content/${template}/**/*.md`);
+
+  return {
+    paths: items.map((file) => {
+      return { params: { slug: fileToUrl(template, file) } };
+    }),
+    fallback: true,
+  };
+};
+
+export const getStaticPropsUtil = async ({ template, params }) => {
+  const path = `content/${template}/${params.slug}.md`;
+  const client = new ForestryClient();
+  const response = await client.getContent<DocumentUnion>({
+    path,
+  });
+
+  return { props: { path, response } };
+};
