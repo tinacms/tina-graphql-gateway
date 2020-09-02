d76c7eb48c4678a7ee010223c6988bd41b6e26a3
James O'Halloran <james.p.ohalloran@gmail.com>
2020-09-02 10:13:55 -0300

    split out generator into own file

--- packages/client/src/auth/authenticate.ts
+++ packages/client/src/auth/authenticate.ts
@@ -17,45 +17,10 @@ limitations under the License.
 */
 
 import popupWindow from "./popupWindow";
-import * as crypto from "crypto-js";
+import { useGenerator } from "./useGenerator";
 
 const TINA_AUTH_CONFIG = "tina_auth_config";
 const SITE_REDIRECT_URI = "http://localhost:2999/authenticating";
-
-const randomString = (length: number = 40) => {
-  let state = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < length; i++)
-    state += possible.charAt(Math.floor(Math.random() * possible.length));
-  return state;
-};
-
-function generateCodeVerifier() {
-  return randomString(128);
-}
-
-function base64URL(string) {
-  crypto.enc.Base64.stringify(string);
-  return string
-    .toString(crypto.enc.Base64)
-    .replace(/=/g, "")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-}
-
-function generateCodeChallenge(code_verifier) {
-  return base64URL(crypto.SHA256(code_verifier));
-}
-
-export const useGenerator = () => {
-  const codeVerifier = generateCodeVerifier();
-  return {
-    state: randomString(),
-    codeChallenge: generateCodeChallenge(codeVerifier),
-    codeVerifier,
-  };
-};
 
 export const authenticate = (
   clientId: string,
