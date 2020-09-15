8a3308240c788fece6e64f2cf10999585127548a
Jeff See <jeffsee.55@gmail.com>
2020-09-15 10:32:09 -0700

    get schema tests setup

---
+++ packages/gql/src/schema-builder.spec.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import fs from "fs";
+import { schemaBuilder } from "./schema-builder";
+import { FilesystemDataSource } from "./datasources/filesystem-manager";
+import { printSchema } from "graphql";
+
+describe("Schema builder", () => {
+  test("does it", async () => {
+    const projectRoot = path.join(process.cwd(), "src/fixtures/project1");
+    const output = await fs
+      .readFileSync(path.join(projectRoot, "schema.gql"))
+      .toString();
+
+    const datasource = FilesystemDataSource(projectRoot);
+    const schema = await schemaBuilder({ datasource });
+    expect(printSchema(schema)).toEqual(output);
+  });
+});
