537f377462d13675c9bae47dcdba3f9d6514298f
Jeff See <jeffsee.55@gmail.com>
2020-09-23 14:03:54 -0700

    Commit before eject cra graphiql

--- apps/graphiql/src/App.tsx
+++ apps/graphiql/src/App.tsx
@@ -3,14 +3,41 @@ import { Explorer } from "./graphiql";
 import "./App.css";
 import "graphiql/graphiql.css";
 import "codemirror/lib/codemirror.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 interface AppProps {}
 
 // const url = "http://localhost:4004/api/graphql"
-const url = "http://localhost:4000/me";
+const url = "http://localhost:4000/project1";
 
 function App({}: AppProps) {
-  return <Explorer url={url} />;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Project 1</Link>
+            </li>
+            <li>
+              <Link to="/project2">Project 2</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/project2">
+            <Explorer path="project2" />;
+          </Route>
+          <Route path="/">
+            <Explorer path="project1" />;
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
