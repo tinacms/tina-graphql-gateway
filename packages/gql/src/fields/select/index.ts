5a96cc2f80c598addafa0eab4f0a58ca520b0d4f
Jeff See <jeffsee.55@gmail.com>
2020-10-13 20:17:33 -0700

    Get working with recursive schema

--- packages/gql/src/fields/select/index.ts
+++ packages/gql/src/fields/select/index.ts
@@ -69,6 +69,7 @@ export const select = {
             cache,
             section: select.config.source.section,
             accumulator,
+            build: false,
           });
 
           return {
