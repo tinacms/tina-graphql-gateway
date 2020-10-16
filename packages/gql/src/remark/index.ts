de64e2d886171d9b23000572b037c52c7e877047
Jeff See <jeffsee.55@gmail.com>
2020-10-16 14:09:35 -0700

    Use remove-position plugin for markdown

--- packages/gql/src/remark/index.ts
+++ packages/gql/src/remark/index.ts
@@ -1,14 +1,11 @@
-const unified = require("unified");
-const remark2rehype = require("remark-rehype");
-const html = require("rehype-stringify");
-const markdown = require("remark-parse");
+import unified from "unified";
+import remark2rehype from "remark-rehype";
+import html from "rehype-stringify";
+import markdown from "remark-parse";
+import removePosition from "unist-util-remove-position";
 
 export const toHTML = async ({ contents: c }: { contents: string }) => {
-  var compiler = unified()
-    .use(markdown)
-    .use({ settings: { position: false } })
-    .use(remark2rehype)
-    .use(html);
+  var compiler = unified().use(markdown).use(remark2rehype).use(html);
 
   const { contents } = compiler.processSync({ contents: c });
 
@@ -16,10 +13,9 @@ export const toHTML = async ({ contents: c }: { contents: string }) => {
 };
 
 export const toAst = async ({ contents }: { contents: string }) => {
-  var tree = unified()
-    .use(markdown)
-    .use({ settings: { position: false } })
-    .parse(contents);
+  var tree = unified().use(markdown, {}).parse(contents);
+
+  removePosition(tree, true);
 
   return tree;
 };
