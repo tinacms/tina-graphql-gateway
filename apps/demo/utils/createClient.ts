4ad90c57032fa066c453b8efca63f24acb8369af
James O'Halloran <james.p.ohalloran@gmail.com>
2020-11-04 11:40:59 -0400

    add comment

--- apps/demo/utils/createClient.ts
+++ apps/demo/utils/createClient.ts
@@ -4,7 +4,7 @@ export const createClient = (editMode: boolean) => {
   const options = editMode ? {
     gqlServer: `http://localhost:3003/github/${process.env.SITE_CLIENT_ID}`,
   } : {
-    
+    // use the default for local files
   }
   
   return new ForestryClient(process.env.SITE_CLIENT_ID, options);
