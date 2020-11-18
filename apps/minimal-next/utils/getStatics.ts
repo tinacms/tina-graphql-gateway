4619751f9d0b341b3de4e6c800c8ef1f6179ac9e
Jeff See <jeffsee.55@gmail.com>
2020-11-18 10:16:08 -0800

    Reset query when file name changes in graphiql

--- apps/minimal-next/utils/getStatics.ts
+++ apps/minimal-next/utils/getStatics.ts
@@ -11,45 +11,9 @@ export const getSlugs = async ({ template }) => {
 export const getContent = async ({ template, params }) => {
   const relativePath = `${params.slug.join("/")}.md`;
   const section = template;
-  const content = await client.requestWithForm({
-    query: gql`
-      query DocumentQuery($relativePath: String!, $section: String!) {
-        document(relativePath: $relativePath, section: $section) {
-          node {
-            __typename
-            ... on Post {
-              data {
-                title
-                author {
-                  section {
-                    type
-                    path
-                    label
-                    create
-                    match
-                    templates
-                    slug
-                  }
-                  path
-                  relativePath
-                  breadcrumbs
-                  basename
-                  extension
-                  filename
-                  node {
-                    ... on Author {
-                      __typename
-                    }
-                  }
-                }
-                hashtags
-              }
-            }
-          }
-        }
-      }
-    `,
-    variables: { relativePath, section },
+  const content = await client.getContentForSection<object>({
+    relativePath,
+    section,
   });
   return {
     ...content,
