713dfbb1ae78bf39a1c0d3b3f854ddf295232739
James O'Halloran <james.p.ohalloran@gmail.com>
2020-12-03 10:30:17 -0400

    Use cognito auth as default

--- packages/client/src/client/index.ts
+++ packages/client/src/client/index.ts
@@ -38,15 +38,15 @@ interface ServerOptions {
 export class ForestryClient {
   serverURL: string;
   oauthHost: string;
-  identityHost: string;
   clientId: string;
   query: string;
   redirectURI: string
   getToken: () => string
   constructor(options: ServerOptions) {
+
+
     this.serverURL = options.customAPI || `https://content.tinajs.dev/github/${options.realm}/${options.clientId}`,
-    this.oauthHost = process.env.TINAIO_OAUTH_HOST || `https://tina-auth-${options.realm}.${REACT_APP_USER_POOL_DASHBOARD_DOMAIN_SUFFIX}/login`;
-    this.identityHost = options.identityProxy || DEFAULT_IDENTITY_HOST;
+    this.oauthHost = process.env.TINAIO_OAUTH_HOST || `https://tina-auth-${options.realm}.${REACT_APP_USER_POOL_DASHBOARD_DOMAIN_SUFFIX}`;
     this.redirectURI = options.redirectURI
     this.clientId = options.clientId;
 
@@ -222,7 +222,7 @@ export class ForestryClient {
   }
 
   async getUser() {
-    const url = `${this.identityHost}/me`;
+    const url = `${this.oauthHost}/oauth2/userInfo`;
 
     try {
       const res = await fetch(url, {
