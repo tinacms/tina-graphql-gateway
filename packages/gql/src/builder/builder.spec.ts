3ef091da9333cf3fb8416e83ef944ac78db3d2de
Jeff See <jeffsee.55@gmail.com>
2020-12-15 13:55:04 -0800

    Add test back for schema builder

--- packages/gql/src/builder/builder.spec.ts
+++ packages/gql/src/builder/builder.spec.ts
@@ -12,258 +12,31 @@ const sleep = (milliseconds: number) => {
 
 describe("Schema builder", () => {
   test("does it", async () => {
-    const projectRoot = path.join(process.cwd(), "src/fixtures/basic");
-
-    const datasource = new FileSystemManager(projectRoot);
-    const cache = cacheInit(datasource);
-    const schema = await builder.schema({ cache });
-
-    // await fs.writeFileSync(
-    //   path.join(projectRoot, "ast-schema.json"),
-    //   JSON.stringify(schema, null, 2)
-    // );
-    await fs.writeFileSync(
-      path.join(projectRoot, "ast-schema.graphql"),
-      printSchema(buildASTSchema(schema))
+    const projectRoot = path.join(process.cwd(), "src/fixtures/demo");
+    const fixtures: string[] = await fs.readdirSync(
+      path.join(process.cwd(), "src/fixtures")
     );
 
-    // await fs.writeFileSync(
-    //   path.join(projectRoot, "refactor.graphql"),
-    //   printSchema(schema)
-    // );
+    await Promise.all(
+      fixtures
+        .filter((f) => f !== ".DS_Store")
+        .map(async (f) => {
+          const projectRoot = path.join(process.cwd(), `src/fixtures/${f}`);
+          const datasource = new FileSystemManager(projectRoot);
+          const cache = cacheInit(datasource);
+          const { schema } = await builder.schema({ cache });
 
-    // assertSchema(schema).matches(gql`
-    //   type Query {
-    //     document(path: String): DocumentUnion
-    //   }
+          const schemaString = printSchema(buildASTSchema(schema));
 
-    //   union DocumentUnion = Post | Author
+          // await fs
+          //   .writeFileSync(path.join(projectRoot, "ast-schema.graphql"), schemaString)
 
-    //   type Post {
-    //     path: String
-    //     form: PostForm
-    //     data: PostData
-    //     initialValues: PostInitialValues
-    //   }
+          const schemaFile = await fs
+            .readFileSync(path.join(projectRoot, "ast-schema.graphql"))
+            .toString();
 
-    //   type PostForm {
-    //     label: String
-    //     _template: String
-    //     fields: [PostFormFields]
-    //   }
-
-    //   union PostFormFields =
-    //       TextareaField
-    //     | SelectField
-    //     | PostSectionsBlocksField
-
-    //   type TextareaField {
-    //     name: String
-    //     label: String
-    //     component: String
-    //     description: String
-    //   }
-
-    //   type SelectField {
-    //     name: String
-    //     label: String
-    //     component: String
-    //     options: [String]
-    //   }
-
-    //   type PostSectionsBlocksField {
-    //     name: String
-    //     label: String
-    //     component: String
-    //     templates: PostSectionsBlocksFieldTemplates
-    //   }
-
-    //   type PostSectionsBlocksFieldTemplates {
-    //     Section: SectionForm
-    //   }
-
-    //   type SectionForm {
-    //     label: String
-    //     _template: String
-    //     fields: [SectionFormFields]
-    //   }
-
-    //   union SectionFormFields = TextareaField | SectionCtaGroupField | ListField
-
-    //   type SectionCtaGroupField {
-    //     name: String
-    //     label: String
-    //     component: String
-    //     fields: [SectionCtaFormFields]
-    //   }
-
-    //   union SectionCtaFormFields = TextareaField
-
-    //   type ListField {
-    //     name: String
-    //     label: String
-    //     component: String
-    //     field: ListFormFieldItemField
-    //   }
-
-    //   union ListFormFieldItemField = SelectField | TextField
-
-    //   type TextField {
-    //     component: String
-    //   }
-
-    //   type PostData {
-    //     title: String
-    //     author: AuthorDocument
-    //     sections: [sectionDataUnion]
-    //   }
-
-    //   type AuthorDocument {
-    //     document: authorsDocumentUnion
-    //   }
-
-    //   union authorsDocumentUnion = Author
-
-    //   type Author {
-    //     path: String
-    //     form: AuthorForm
-    //     data: AuthorData
-    //     initialValues: AuthorInitialValues
-    //   }
-
-    //   type AuthorForm {
-    //     label: String
-    //     _template: String
-    //     fields: [AuthorFormFields]
-    //   }
-
-    //   union AuthorFormFields =
-    //       TextareaField
-    //     | SelectField
-    //     | ListField
-    //     | AuthorAccoladesGroupListField
-
-    //   type AuthorAccoladesGroupListField {
-    //     name: String
-    //     label: String
-    //     component: String
-    //     fields: [AuthorAccoladesFormFields]
-    //   }
-
-    //   union AuthorAccoladesFormFields = TextareaField
-
-    //   type AuthorData {
-    //     name: String
-    //     role: String
-    //     anecdotes: [String]
-    //     accolades: [AccoladesData]
-    //   }
-
-    //   type AccoladesData {
-    //     figure: String
-    //     description: String
-    //   }
-
-    //   type AuthorInitialValues {
-    //     _template: String
-    //     name: String
-    //     role: String
-    //     anecdotes: [String]
-    //     accolades: [AccoladesData]
-    //   }
-
-    //   union sectionDataUnion = SectionData
-
-    //   type SectionData {
-    //     description: String
-    //     cta: CtaData
-    //     authors: AuthorsDocuments
-    //   }
-
-    //   type CtaData {
-    //     header: String
-    //   }
-
-    //   type AuthorsDocuments {
-    //     documents: [authorsDocumentUnion]
-    //   }
-
-    //   type PostInitialValues {
-    //     _template: String
-    //     title: String
-    //     author: String
-    //     sections: [sectionInitialValuesUnion]
-    //   }
-
-    //   union sectionInitialValuesUnion = SectionInitialValues
-
-    //   type SectionInitialValues {
-    //     _template: String
-    //     description: String
-    //     authors: [String]
-    //     cta: CtaInitialValues
-    //   }
-
-    //   type CtaInitialValues {
-    //     _template: String
-    //     header: String
-    //   }
-
-    //   type Mutation {
-    //     updateDocument(path: String!, params: DocumentInput): DocumentUnion
-    //   }
-
-    //   input DocumentInput {
-    //     PostInput: PostInput
-    //     AuthorInput: AuthorInput
-    //   }
-
-    //   input PostInput {
-    //     content: String
-    //     data: PostInputData
-    //   }
-
-    //   input PostInputData {
-    //     _template: String
-    //     title: String
-    //     author: String
-    //     sections: [PostSectionsBlocksInput]
-    //   }
-
-    //   input PostSectionsBlocksInput {
-    //     SectionInputData: SectionInputData
-    //   }
-
-    //   input SectionInputData {
-    //     _template: String
-    //     description: String
-    //     authors: [String]
-    //     cta: CtaInputData
-    //   }
-
-    //   input CtaInputData {
-    //     _template: String
-    //     header: String
-    //   }
-
-    //   input AuthorInput {
-    //     content: String
-    //     data: AuthorInputData
-    //   }
-
-    //   input AuthorInputData {
-    //     _template: String
-    //     name: String
-    //     role: String
-    //     anecdotes: [String]
-    //     accolades: [AccoladesInputData]
-    //   }
-
-    //   input AccoladesInputData {
-    //     _template: String
-    //     figure: String
-    //     description: String
-    //   }
-    // `);
+          expect(schemaFile).toEqual(schemaString);
+        })
+    );
   });
 });
