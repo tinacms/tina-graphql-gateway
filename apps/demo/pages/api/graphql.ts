575b59058a77b068badae5724b7c2e66ad6b94b5
Jeff See <jeffsee.55@gmail.com>
2021-01-05 12:46:15 -0800

    Logs

--- apps/demo/pages/api/graphql.ts
+++ apps/demo/pages/api/graphql.ts
@@ -6,6 +6,8 @@ export default async function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
 
+  console.log("cw", process.cwd());
+
   const json = await gql({
     projectRoot: ".",
     query,
