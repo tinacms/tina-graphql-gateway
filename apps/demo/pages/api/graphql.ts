65663f06e49b7e1088a6eca9fe1b38610b7c0633
Jeff See <jeffsee.55@gmail.com>
2021-01-05 12:49:15 -0800

    Logging

--- apps/demo/pages/api/graphql.ts
+++ apps/demo/pages/api/graphql.ts
@@ -1,4 +1,6 @@
 import { gql } from "@forestryio/gql";
+import fs from "fs";
+import path from "path";
 
 export default async function handler(req, res) {
   const { query, variables } = req.body;
@@ -7,6 +9,8 @@ export default async function handler(req, res) {
   res.setHeader("Content-Type", "application/json");
 
   console.log("cw", process.cwd());
+  console.log("dir", await fs.readdirSync(process.cwd()));
+  console.log("path", await fs.readdirSync(path.join(process.cwd(), ".tina")));
 
   const json = await gql({
     projectRoot: ".",
