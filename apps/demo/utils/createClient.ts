fb844229ce76e0055377a634290925d885983f0b
Jeff See <jeffsee.55@gmail.com>
2021-01-05 15:50:31 -0800

    Add env vars to next config

--- apps/demo/utils/createClient.ts
+++ apps/demo/utils/createClient.ts
@@ -3,17 +3,12 @@ import {
   DEFAULT_LOCAL_TINA_GQL_SERVER_URL,
 } from "@forestryio/client";
 
-console.log("deployedURL", process.env.DEPLOYED_URL);
-
-console.log("vercel URL", process.env.VERCEL_URL);
-
 export const createClient = (editMode: boolean) => {
-  console.log(process.env);
   const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
   console.log("a", process.env.DEPLOYED_URL);
   console.log("b", process.env.VERCEL_URL);
   const customAPI = process.env.DEPLOYED_URL
-    ? `https://${process.env.DEPLOYED_URL}`
+    ? `${protocol}://${process.env.DEPLOYED_URL}`
     : "http://localhost:2999/api/graphql";
 
   console.log("capi", customAPI);
