7fc47b7f2b3ad3204f878776d3a408ef5cf0c855
Jeff See <jeffsee.55@gmail.com>
2021-01-05 11:51:10 -0800

    temporary setup custom url

---
+++ apps/demo/pages/api/graphql.ts
@@ -0,0 +1,15 @@
+import { gql } from "@forestryio/gql";
+
+export default async function handler(req, res) {
+  const { query, variables } = req.body;
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+
+  const json = await gql({
+    projectRoot: ".",
+    query,
+    variables,
+  });
+  res.end(JSON.stringify(json));
+}
