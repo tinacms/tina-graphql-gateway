5e2fbbb02b57c30db34f575e44b8c1ca6ef10c5c
Jeff See <jeffsee.55@gmail.com>
2021-01-06 14:25:24 -0800

    Fix test

--- packages/graphql-helpers/src/util.spec.ts
+++ packages/graphql-helpers/src/util.spec.ts
@@ -3,12 +3,12 @@ import * as utils from "./util";
 describe("friendlyName", () => {
   test("it should convert to CamelCase", () => {
     const input = "action-newsletter.yml";
-    const output = "ActionNewsletter";
+    const output = "ActionNewsletterYml";
     expect(utils.friendlyName(input)).toEqual(output);
   });
   test("it should add suffix", () => {
     const input = "action-newsletter.yml";
-    const output = "ActionNewsletterSuffix";
+    const output = "ActionNewsletter_suffix";
     expect(utils.friendlyName(input, { suffix: "suffix" })).toEqual(output);
   });
 });
