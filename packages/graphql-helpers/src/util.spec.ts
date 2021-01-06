9aef74eb6047ddd31f486a4bc5943c0c1c6822a1
Jeff See <jeffsee.55@gmail.com>
2021-01-06 14:20:48 -0800

    Remove unsused function from test

--- packages/graphql-helpers/src/util.spec.ts
+++ packages/graphql-helpers/src/util.spec.ts
@@ -1,18 +1,5 @@
 import * as utils from "./util";
 
-describe("shortFMTName", () => {
-  test("it should remove the extension", () => {
-    const input = "action-newsletter.yml";
-    const output = "action-newsletter";
-    expect(utils.shortFMTName(input)).toEqual(output);
-  });
-  test("it should remove the FMT base path", () => {
-    const input = `${utils.FMT_BASE}/action-newsletter.yml`;
-    const output = "action-newsletter";
-    expect(utils.shortFMTName(input)).toEqual(output);
-  });
-});
-
 describe("friendlyName", () => {
   test("it should convert to CamelCase", () => {
     const input = "action-newsletter.yml";
