1e7bee7c9113946e6cca04c7a805df22dea907f5
Jeff See <jeffsee.55@gmail.com>
2021-01-05 16:41:15 -0800

    Remove logs

--- apps/demo/utils/createClient.ts
+++ apps/demo/utils/createClient.ts
@@ -5,9 +5,6 @@ import {
 
 export const createClient = (editMode: boolean) => {
   const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-  console.log("protocol", process.env.NODE_ENV);
-  console.log("a", process.env.DEPLOYED_URL);
-  console.log("b", process.env.VERCEL_URL);
   const customAPI = process.env.DEPLOYED_URL
     ? `${protocol}://${process.env.DEPLOYED_URL}/api/graphql`
     : "http://localhost:2999/api/graphql";
