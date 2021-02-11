77083ee0c2022b591d5ff4d0b09dcac2c32a04bc
Jeff See <jeffsee.55@gmail.com>
2021-02-11 15:28:08 -0800

    Undo unnecessary changes

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -36,9 +36,9 @@ module.exports = {
     /**
      * Enable these when you want to work with Tina locally
      */
-    config.resolve.alias["@tinacms"] = path.resolve(
-      "../../../tinacms/packages/@tinacms"
-    );
+    // config.resolve.alias["@tinacms"] = path.resolve(
+    //   "../../../tinacms/packages/@tinacms"
+    // );
     config.resolve.alias["tinacms"] = require.resolve("tinacms");
     config.resolve.alias["react-dom"] = require.resolve("react-dom");
     config.resolve.alias["react"] = require.resolve("react");
