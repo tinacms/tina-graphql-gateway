5dcdafb41e9f393a703dc78ea67029c43790d5cc
Jeff See <jeffsee.55@gmail.com>
2021-02-12 10:41:36 -0800

    Move temp workaround into its own file

--- packages/client/src/hooks/form-service.ts
+++ packages/client/src/hooks/form-service.ts
@@ -20,11 +20,10 @@ import {
 } from "xstate";
 import { splitDataNode } from "@forestryio/graphql-helpers";
 import { Form, TinaCMS } from "tinacms";
-import finalFormArrays from "final-form-arrays";
-import { getIn, setIn } from "final-form";
 
 import type { Client } from "../client";
 import type { DocumentNode } from "./use-form";
+import { fixMutators } from "./temporary-fix-mutators";
 
 export const createFormMachine = (initialContext: {
   queryFieldName: string;
@@ -95,7 +94,7 @@ export const createFormMachine = (initialContext: {
   });
 };
 
-type NodeFormContext = {
+export type NodeFormContext = {
   queryFieldName: string;
   queryString: string;
   node: DocumentNode;
@@ -114,7 +113,7 @@ type NodeFormContext = {
   onSubmit: (args: any) => void;
 };
 
-type NodeFormEvent = {
+export type NodeFormEvent = {
   type: "ON_FIELD_CHANGE";
   values: { path: (string | number)[]; value: unknown };
 };
@@ -292,123 +291,11 @@ ${mutation}
 
   /**
    *
-   * The following changes to final-form mutators are not intended to
-   * be a long-term solution. The goal of these changes is to enable
-   * the ability to "subscribe" to field-level changes for array mutations.
-   *
-   * An example of this is that we don't get any sort of event from Tina
-   * when a new block or list item is added, rearranged, or removed. This
-   * makes it impossible for us to keep the `data` of our GraphQL response
-   * in-sync with form state.
-   *
-   * A long-term solution for this should be discussed in this ticket
-   * https://github.com/tinacms/tinacms/issues/1669
+   * HEADS UP this should be removed when
+   * https://github.com/tinacms/tinacms/issues/1669 is resolved
    *
    */
-  const changeValue = (state, name, mutate) => {
-    const before = getIn(state.formState.values, name);
-    const after = mutate(before);
-    state.formState.values = setIn(state.formState.values, name, after) || {};
-  };
-
-  const { move: moveCopy, remove: removeCopy, insert: insertCopy } = {
-    ...form.finalForm.mutators,
-  };
-  form.finalForm.mutators.move = (name, from, to) => {
-    const dataValue = getIn(context.node.data, name);
-    let state = {
-      formState: { values: { fakeValue: dataValue } },
-    };
-    try {
-      // @ts-ignore state is expecting the full final-form state, but we don't need it
-      finalFormArrays.move(["fakeValue", from, to], state, { changeValue });
-      // FIXME: this throws an error, probably because of "state" but the mutation works :shrug:
-    } catch (e) {
-      callback({
-        type: "ON_FIELD_CHANGE",
-        values: {
-          path: [context.queryFieldName, "data", ...name.split(".")],
-          value: state.formState.values.fakeValue,
-        },
-      });
-    }
-
-    // Return the copy like nothing ever happened
-    return moveCopy(name, from, to);
-  };
-
-  form.finalForm.mutators.remove = (name, index) => {
-    const dataValue = getIn(context.node.data, name);
-    let state = {
-      formState: { values: { fakeValue: dataValue } },
-    };
-    try {
-      // @ts-ignore state is expecting the full final-form state, but we don't need it
-      finalFormArrays.remove(["fakeValue", index], state, { changeValue });
-      // FIXME: this throws an error, probably because of "state" but the mutation works :shrug:
-    } catch (e) {
-      callback({
-        type: "ON_FIELD_CHANGE",
-        values: {
-          path: [context.queryFieldName, "data", ...name.split(".")],
-          value: state.formState.values.fakeValue,
-        },
-      });
-    }
-
-    // Return the copy like nothing ever happened
-    return removeCopy(name, index);
-  };
-
-  form.finalForm.mutators.insert = (name, index, item) => {
-    const dataValue = getIn(context.node.data, name);
-    let state = {
-      formState: { values: { fakeValue: dataValue } },
-    };
-    try {
-      let newItem = item;
-      // FIXME: this is a pretty rough translation, not sure if "_Data" would be present in all cases
-      // This should be abstracted in to graphql-helpers so we can commonize these transforms
-      if (item._template) {
-        newItem = {
-          __typename:
-            item._template.charAt(0).toUpperCase() +
-            item._template.slice(1) +
-            "_Data",
-        };
-      } else {
-        // if item is -> {}, the real insertCopy doesn't set up the event listeners properly
-        // so inputs within the added field won't work for some reason
-        if (
-          item &&
-          Object.keys(item).length === 0 &&
-          item.constructor === Object
-        ) {
-          item = null;
-        }
-      }
-      finalFormArrays.insert(
-        ["fakeValue", index, newItem],
-        // @ts-ignore state is expecting the full final-form state, but we don't need it
-        state,
-        {
-          changeValue,
-        }
-      );
-      // FIXME: this throws an error, probably because of "state" but the mutation works :shrug:
-    } catch (e) {
-      callback({
-        type: "ON_FIELD_CHANGE",
-        values: {
-          path: [context.queryFieldName, "data", ...name.split(".")],
-          value: state.formState.values.fakeValue,
-        },
-      });
-    }
-
-    // Return the copy like nothing ever happened
-    return insertCopy(name, index, item);
-  };
+  fixMutators({ context, form, callback });
 
   form.subscribe(
     (all) => {
