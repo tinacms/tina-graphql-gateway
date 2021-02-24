5b27e5d45a59de9f09cfc1219f84166c381e1d08
Jeff See <jeffsee.55@gmail.com>
2021-02-23 19:10:34 -0800

    Fix types for gql test-util

--- packages/graphql-helpers/src/test-util.ts
+++ packages/graphql-helpers/src/test-util.ts
@@ -6,32 +6,8 @@ import prettier from "prettier";
  * due to differences in whitespace, calling it "gql" has the nice
  * side-effect that IDEs often provide graphql syntax highlighting for it
  */
-export function gql(
-  strings: TemplateStringsArray,
-  ...values: string[]
-): string {
-  let str = "";
-
-  for (let i = 0; i < strings.length; ++i) {
-    str += strings[i];
-    if (i < values.length) {
-      const value = values[i];
-
-      str += value;
-    }
-  }
-  const trimmedStr = str
-    .replace(/^\n*/m, "") //  remove leading newline
-    .replace(/[ \t]*$/, ""); // remove trailing spaces and tabs
-
-  // fixes indentation by removing leading spaces and tabs from each line
-  let indent = "";
-  for (const char of trimmedStr) {
-    if (char !== " " && char !== "\t") {
-      break;
-    }
-    indent += char;
-  }
-  const string = trimmedStr.replace(RegExp("^" + indent, "mg"), ""); // remove indent
+export function gql(strings: TemplateStringsArray | string[]): string {
+  const string = Array.isArray(strings) ? strings.join("\n") : strings;
+  // @ts-ignore
   return prettier.format(string, { parser: "graphql" });
 }
