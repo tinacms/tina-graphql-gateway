2f58a238f9080541e2421f7d30f6ea5699e2d677
Logan Anderson <landerson2@upei.ca>
2021-05-26 15:13:09 -0300

    update error message

---
+++ packages/tina-graphql-gateway-cli/src/cmds/compile/defaultSchema.ts
@@ -0,0 +1,56 @@
+export const defaultSchema = `
+import { defineSchema } from "tina-graphql-gateway-cli";
+
+export default defineSchema({
+  collections: [
+    {
+      label: "Blog Posts",
+      name: "posts",
+      path: "content/posts",
+      templates: [
+        {
+          label: "Article",
+          name: "article",
+          fields: [
+            {
+              type: "text",
+              label: "Title",
+              name: "title",
+            },
+            {
+              type: "reference",
+              label: "Author",
+              name: "author",
+              collection: "authors",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Authors",
+      name: "authors",
+      path: "content/authors",
+      templates: [
+        {
+          label: "Author",
+          name: "author",
+          fields: [
+            {
+              type: "text",
+              label: "Name",
+              name: "name",
+            },
+            {
+              type: "text",
+              label: "Avatar",
+              name: "avatar",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
+
+`;
